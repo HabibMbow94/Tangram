124004b6349b953edfd6284b65eb9bc704dc7505
Tangram59 <animetangram202159@gmail.com>
2021-08-17 15:07:28 +0200

    check the code

--- index.js
+++ index.js
@@ -1,103 +1,58 @@
-function index() {
-    // Obtains an array of all <link>
-    // elements.
-    // Select your element using indexing.
-    var theme = document.getElementById("change-style");
+// var theme = document.getElementById("change-style");
+// function index() {
+// 	// Obtains an array of all <link>
+// 	// elements.
+// 	// Select your element using indexing.
+// 	// Change the value of href attribute
+// 	// to change the css sheet.
+// 	if (theme.getAttribute("href") != "Styles/index.css") {
+// 		theme.setAttribute("href", "Styles/index.css");
+// 	} else {
+// 		theme.setAttribute("href", "Styles/index.css");
+// 	}
+// }
+// function square() {
+// 	// Obtains an array of all <link>
+// 	// elements.
+// 	// Select your element using indexing.
+// 	// var theme = document.getElementById("change-style");
 
-    // Change the value of href attribute
-    // to change the css sheet.
-    if (theme.getAttribute("href") != "Styles/index.css") {
-        theme.setAttribute("href", "Styles/index.css");
-    } else {
-        theme.setAttribute("href", "Styles/index.css");
-    }
-}
+// 	// Change the value of href attribute
+// 	// to change the css sheet.
+// 	if (theme.getAttribute("href") != "Styles/square.css") {
+// 		theme.setAttribute("href", "Styles/square.css");
+// 	} else {
+// 		theme.setAttribute("href", "Styles/square.css");
+// 	}
+// }
+// function boat() {
+// 	// Obtains an array of all <link>
+// 	// elements.
+// 	// Select your element using indexing.
+// 	// var theme = document.getElementById("change-style");
 
-function square() {
-    // Obtains an array of all <link>
-    // elements.
-    // Select your element using indexing.
-    var theme = document.getElementById("change-style");
+// 	// Change the value of href attribute
+// 	// to change the css sheet.
+// 	if (theme.getAttribute("href") != "Styles/boat.css") {
+// 		theme.setAttribute("href", "Styles/boat.css");
+// 	} else {
+// 		theme.setAttribute("href", "Styles/boat.css");
+// 	}
+// }
+// 	// Obtains an array of all <link>
+// 	// elements.
+// 	// Select your element using indexing.
+// // var theme = document.getElementById("change-style");
 
-    // Change the value of href attribute
-    // to change the css sheet.
-    if (theme.getAttribute("href") != "Styles/square.css") {
-        theme.setAttribute("href", "Styles/square.css");
-    } else {
-        theme.setAttribute("href", "Styles/square.css");
-    }
-}
+// // Change the value of href attribute
+// // to change the css sheet.
+// if (theme.getAttribute("href") != "Styles/square.css") {
+// 	theme.setAttribute("href", "Styles/square.css");
+// } else {
+// 		theme.setAttribute("href", "Styles/square.css");
+// }
 
-function poule() {
-    // Obtains an array of all <link>
-    // elements.
-    // Select your element using indexing.
-    var theme = document.getElementById("change-style");
-
-    // Change the value of href attribute
-    // to change the css sheet.
-    if (theme.getAttribute("href") != "Styles/poule.css") {
-        theme.setAttribute("href", "Styles/poule.css");
-    } else {
-        theme.setAttribute("href", "Styles/poule.css");
-    }
-}
-
-function pointD() {
-    // Obtains an array of all <link>
-    // elements.
-    // Select your element using indexing.
-    var theme = document.getElementById("change-style");
-
-    // Change the value of href attribute
-    // to change the css sheet.
-    if (theme.getAttribute("href") != "Styles/pointD.css") {
-        theme.setAttribute("href", "Styles/pointD.css");
-    } else {
-        theme.setAttribute("href", "Styles/pointD.css");
-    }
-}
-
-function marteau() {
-    // Obtains an array of all <link>
-    // elements.
-    // Select your element using indexing.
-    var theme = document.getElementById("change-style");
-
-    // Change the value of href attribute
-    // to change the css sheet.
-    if (theme.getAttribute("href") != "Styles/marteau.css") {
-        theme.setAttribute("href", "Styles/marteau.css");
-    } else {
-        theme.setAttribute("href", "Styles/marteau.css");
-    }
-}
-
-function coq() {
-    // Obtains an array of all <link>
-    // elements.
-    // Select your element using indexing.
-    var theme = document.getElementById("change-style");
-
-    // Change the value of href attribute
-    // to change the css sheet.
-    if (theme.getAttribute("href") != "Styles/coq.css") {
-        theme.setAttribute("href", "Styles/coq.css");
-    } else {
-        theme.setAttribute("href", "Styles/coq.css");
-    }
-}
-
-
-// Obtains an array of all <link>
-// elements.
-// Select your element using indexing.
-var theme = document.getElementById("change-style");
-
-// Change the value of href attribute
-// to change the css sheet.
-if (theme.getAttribute("href") != "Styles/square.css") {
-    theme.setAttribute("href", "Styles/square.css");
-} else {
-    theme.setAttribute("href", "Styles/square.css");
+function swap(cssFile) {
+    var link = document.getElementById("style")
+    link.href = cssFile;
 }
