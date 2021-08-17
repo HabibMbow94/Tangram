15d41bbcbf87b1e8f561cd483aa9a0bf64ce091d
Tangram59 <animetangram202159@gmail.com>
2021-08-17 12:21:39 +0200

    My personal commit johann

---
+++ index.js
@@ -0,0 +1,103 @@
+function index() {
+    // Obtains an array of all <link>
+    // elements.
+    // Select your element using indexing.
+    var theme = document.getElementById("change-style");
+
+    // Change the value of href attribute
+    // to change the css sheet.
+    if (theme.getAttribute("href") != "Styles/index.css") {
+        theme.setAttribute("href", "Styles/index.css");
+    } else {
+        theme.setAttribute("href", "Styles/index.css");
+    }
+}
+
+function square() {
+    // Obtains an array of all <link>
+    // elements.
+    // Select your element using indexing.
+    var theme = document.getElementById("change-style");
+
+    // Change the value of href attribute
+    // to change the css sheet.
+    if (theme.getAttribute("href") != "Styles/square.css") {
+        theme.setAttribute("href", "Styles/square.css");
+    } else {
+        theme.setAttribute("href", "Styles/square.css");
+    }
+}
+
+function poule() {
+    // Obtains an array of all <link>
+    // elements.
+    // Select your element using indexing.
+    var theme = document.getElementById("change-style");
+
+    // Change the value of href attribute
+    // to change the css sheet.
+    if (theme.getAttribute("href") != "Styles/poule.css") {
+        theme.setAttribute("href", "Styles/poule.css");
+    } else {
+        theme.setAttribute("href", "Styles/poule.css");
+    }
+}
+
+function pointD() {
+    // Obtains an array of all <link>
+    // elements.
+    // Select your element using indexing.
+    var theme = document.getElementById("change-style");
+
+    // Change the value of href attribute
+    // to change the css sheet.
+    if (theme.getAttribute("href") != "Styles/pointD.css") {
+        theme.setAttribute("href", "Styles/pointD.css");
+    } else {
+        theme.setAttribute("href", "Styles/pointD.css");
+    }
+}
+
+function marteau() {
+    // Obtains an array of all <link>
+    // elements.
+    // Select your element using indexing.
+    var theme = document.getElementById("change-style");
+
+    // Change the value of href attribute
+    // to change the css sheet.
+    if (theme.getAttribute("href") != "Styles/marteau.css") {
+        theme.setAttribute("href", "Styles/marteau.css");
+    } else {
+        theme.setAttribute("href", "Styles/marteau.css");
+    }
+}
+
+function coq() {
+    // Obtains an array of all <link>
+    // elements.
+    // Select your element using indexing.
+    var theme = document.getElementById("change-style");
+
+    // Change the value of href attribute
+    // to change the css sheet.
+    if (theme.getAttribute("href") != "Styles/coq.css") {
+        theme.setAttribute("href", "Styles/coq.css");
+    } else {
+        theme.setAttribute("href", "Styles/coq.css");
+    }
+}
+
+
+// Obtains an array of all <link>
+// elements.
+// Select your element using indexing.
+var theme = document.getElementById("change-style");
+
+// Change the value of href attribute
+// to change the css sheet.
+if (theme.getAttribute("href") != "Styles/square.css") {
+    theme.setAttribute("href", "Styles/square.css");
+} else {
+    theme.setAttribute("href", "Styles/square.css");
+}
