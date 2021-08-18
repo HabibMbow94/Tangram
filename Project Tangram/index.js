85f2fa36eb0041c8f7395c9232dcde008b18375b
‘Ariz <animetangram202159@gmail.com>
2021-08-18 21:16:32 +0200

    third

--- Project Tangram/index.js
+++ Project Tangram/index.js
@@ -2,3 +2,13 @@ function swap(cssFile) {
     var link = document.getElementById("style")
     link.href = cssFile;
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
